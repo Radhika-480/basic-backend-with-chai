43ebe4f2676f6a6ba6500bbda8a7a3dc5ae79582
Radhika-480 <radhikadixit480@gmail.com>
2025-04-27 02:12:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('laddudotcom')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>login page</h1>')
+})
+
+app.get('/youtube',(req,res)=>{
+    res.send('<h2>chai or code</h2>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
